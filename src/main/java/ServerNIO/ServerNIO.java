56da074a7c9317fbbe6ade2c36a9c8bad41ec247
IvanGer212 <Comradeivan88@gmail.com>
2021-07-13 19:35:54 +0300

    Add command "cat", "touch" and "cd" to program.

--- src/main/java/ServerNIO/ServerNIO.java
+++ src/main/java/ServerNIO/ServerNIO.java
@@ -9,18 +9,22 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ServerNIO {
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
     private ByteBuffer buf;
+    private Path root;
 
 
     public ServerNIO() throws IOException {
-
+        root = Paths.get("dir");
         serverSocketChannel = ServerSocketChannel.open();
         selector = Selector.open();
         buf = ByteBuffer.allocate(256);
@@ -66,28 +70,51 @@ public class ServerNIO {
             buf.clear();
         }
         System.out.println("Received: " + builder);
-        String msg = builder.toString();
-        if (msg.equals("ls\r\n")) {
-            String listOfServerFiles = Arrays.toString(new File("dir").list());
-            ByteBuffer response = ByteBuffer.wrap(listOfServerFiles.getBytes(StandardCharsets.UTF_8));
+        String msg = builder.toString().trim();
+        if (msg.equals("ls")) {
+            String files = Files.list(root).map(path -> path.getFileName().toString()).collect(Collectors.joining("\r\n"));
+            ByteBuffer response = ByteBuffer.wrap(files.getBytes(StandardCharsets.UTF_8));
             channel.write(response);
         }
+        else if (msg.startsWith("touch")){
+            String filename = msg.replaceAll("touch ", "").trim();
+            Path newfile = Paths.get(root.toString(),filename);
+            Files.write(newfile, Collections.singletonList(null));
+        }
         else if (msg.startsWith("cat")){
-                String filename = msg.replaceAll("cat ","");
-                byte[] bites = Files.readAllBytes(Paths.get("C:\\Users\\admin\\IdeaProjects\\Test_NIO\\dir\\1.txt")); //(dir,filename));
+                String filename = msg.replaceAll("cat ","").trim();
+                byte[] bites = Files.readAllBytes(Paths.get("dir",filename));
                 String msg1 = new String(bites,"utf8");
                 ByteBuffer response =  ByteBuffer.wrap(msg1.getBytes(StandardCharsets.UTF_8));
                 channel.write(response);
             }
+        else if (msg.startsWith("cd")){
+            String command = msg.replaceAll("cd ","").trim();
+            if (command.equals("..")){
+                root = root.getParent();
+            }
+            else if (Files.isDirectory(root.resolve(command))){
+                root = root.resolve(command);
+            } else {
+                ByteBuffer response = ByteBuffer.wrap((command + " is not directory\r\n").getBytes(StandardCharsets.UTF_8));
+            }
+        }
         else {
             ByteBuffer response = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
             channel.write(response);
         }
+        printPath(channel);
     }
     private void handleAccept(SelectionKey key) throws IOException {
         SocketChannel channel = serverSocketChannel.accept();
         channel.configureBlocking(false);
         channel.register(selector,SelectionKey.OP_READ);
+    }
+
+    private void printPath (SocketChannel channel) throws IOException {
+        String path = root.toString() + " ";
+        ByteBuffer response = ByteBuffer.wrap(path.getBytes(StandardCharsets.UTF_8));
+        channel.write(response);
     }
 
     public static void main(String[] args) throws IOException {
